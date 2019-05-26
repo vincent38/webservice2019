317fff746ccb28bd5c23cf8fd32fadd401a5d17d
misumi3104 <18492524+misumi3104@users.noreply.github.com>
2019-05-27 07:33:33 +0900

    a

--- dev_appserver_redirect.py
+++ dev_appserver_redirect.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-import sys,os
+import os,sys
 for v in os.environ.get("PATH").split(";"):
-	if v.find("google-cloud-sdk")>0:
-		execfile("{0}/dev_appserver.py".format(v))
+	if v.find("google-cloud-sdk")>0 and v.find("bin"):
+		sys.path.append(v)
+		execfile(os.path.join(v,"dev_appserver.py"))
 		break
