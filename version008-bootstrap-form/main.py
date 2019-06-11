eb675d5acae054c2b00a445e70f6fd5150ab99e0
misumi3104 <18492524+misumi3104@users.noreply.github.com>
2019-06-11 18:33:36 +0900

    a

---
+++ version008-bootstrap-form/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#目的　HTMLとJSを書く
+import webapp2,json
+#追加
+from google.appengine.ext.webapp import template
+def hello(request):
+	# return webapp2.Response("hello")
+	return webapp2.Response(template.render("index.html", None))
+def form(request):
+	return webapp2.Response(json.dumps(dict(request.params)))
+app = webapp2.WSGIApplication([('/', hello),("/form",form)])
