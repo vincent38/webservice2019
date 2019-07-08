5abb46ca5cf9eab97e6540eed9626c2a4882937f
misumi3104 <18492524+misumi3104@users.noreply.github.com>
2019-07-08 19:31:54 +0900

    version009

---
+++ version009-datastore2/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#目的　HTMLとJSを書く
+import webapp2,json
+#追加
+from google.appengine.ext.webapp import template
+from google.appengine.ext import blobstore, ndb
+
+class candidate(ndb.Model):
+	born = ndb.DateTimeProperty(auto_now_add=True)
+	vote = ndb.IntegerProperty()
+	name = ndb.StringProperty()
+
+def index(request):
+	keyword=request.get("keyword")
+	if keyword:
+		candidatelist=candidate.query(candidate.name==keyword)
+	else:
+		candidatelist=candidate.query()
+	return webapp2.Response(template.render("index.html", {"candidate":candidatelist.order(-candidate.vote).fetch(),"keyword":keyword}))
+def command(request):
+	id=int(request.get("id") or 0) #idを取得
+	action=request.get("action") #指定された動作を文字列で受けとる
+	if None:
+		pass # 以下で動作を分岐
+	elif action == "upvote": #得票数を増やす
+		m=candidate.get_by_id(id) #idからエンティティを取得
+		m.vote=(m.vote or 0)+1
+		m.put()
+	elif action == "downvote": #得票数を減らす
+		m=candidate.get_by_id(id) #idからエンティティを取得
+		m.vote=(m.vote or 0)-1
+		m.put()
+	elif action == "edit": #編集
+		m=(id and candidate.get_by_id(id)) or candidate() #idからエンティティを取得
+		m.populate(vote=int(request.get("vote") or 0),name=str(request.get("name")))
+		m.put()
+	elif action == "delete":
+		candidate.get_by_id(id).key.delete()
+	return webapp2.redirect("/")
+app = webapp2.WSGIApplication([('/', index),('/command',command)])
